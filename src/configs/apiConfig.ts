bf6f5eb5fecaa2b47026ac59d443084c576e1f73
Camilo Eduardo Montupil Zamorano <40744767+camiloJTG@users.noreply.github.com>
2023-04-15 06:18:25 +0000

    feat: Add endpoint configurations

--- src/configs/apiConfig.ts
+++ src/configs/apiConfig.ts
@@ -2,6 +2,7 @@ const API = process.env.NEXT_PUBLIC_LINK_API_URL_BASE
 
 export const apiConfig = {
   auth: {
-    register: `${API}/user/register`
+    register: `${API}/user/register`,
+    login: `${API}/auth/login`
   }
 }
